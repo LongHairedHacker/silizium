50364f2ae9768e26b16ff82e73f66b52b9424706
LongHairedHacker <sebastian@sebastians-site.de>
2016-06-30 21:01:03 +0200

    Cleaned up setup.sql

--- setup.sql
+++ setup.sql
@@ -2,36 +2,26 @@
 
 -- DROP DATABASE silizium;
 CREATE DATABASE silizium
-  WITH OWNER = silizium
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
-       CONNECTION LIMIT = -1;
+    WITH OWNER = silizium
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    CONNECTION LIMIT = -1;
 
 
--- Table: public.messages
+-- Sequence: public.topics_id_seq
 
--- DROP TABLE public.messages;
+-- DROP SEQUENCE public.topics_id_seq;
 
-CREATE TABLE public.messages
-(
- value double precision NOT NULL,
- "time" timestamp with time zone NOT NULL,
- topic_id integer NOT NULL,
- CONSTRAINT messages_pkey PRIMARY KEY (topic_id, "time"),
- CONSTRAINT messages_topic_id_fkey FOREIGN KEY (topic_id)
-     REFERENCES public.topics (id) MATCH SIMPLE
-     ON UPDATE CASCADE ON DELETE CASCADE
-)
-WITH (
- OIDS=FALSE,
- autovacuum_enabled=true
-);
-
-ALTER TABLE public.messages
-  OWNER TO silizium;
-
+CREATE SEQUENCE public.topics_id_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 25
+    CACHE 1;
+ALTER TABLE public.topics_id_seq
+    OWNER TO silizium;
 
 
 -- Table: public.topics
@@ -40,13 +30,35 @@ ALTER TABLE public.messages
 
 CREATE TABLE public.topics
 (
-id integer NOT NULL DEFAULT nextval('topics_id_seq'::regclass),
-topic character varying NOT NULL,
-CONSTRAINT topics_pkey PRIMARY KEY (id),
-CONSTRAINT topics_topic_key UNIQUE (topic)
+    id integer NOT NULL DEFAULT nextval('topics_id_seq'::regclass),
+    topic character varying NOT NULL,
+    CONSTRAINT topics_pkey PRIMARY KEY (id),
+    CONSTRAINT topics_topic_key UNIQUE (topic)
 )
 WITH (
-OIDS=FALSE
+    OIDS=FALSE
 );
 ALTER TABLE public.topics
-OWNER TO silizium;
+    OWNER TO silizium;
+
+-- Table: public.messages
+
+-- DROP TABLE public.messages;
+
+CREATE TABLE public.messages
+(
+    value double precision NOT NULL,
+    "time" timestamp with time zone NOT NULL,
+    topic_id integer NOT NULL,
+        CONSTRAINT messages_pkey PRIMARY KEY (topic_id, "time"),
+        CONSTRAINT messages_topic_id_fkey FOREIGN KEY (topic_id)
+        REFERENCES public.topics (id) MATCH SIMPLE
+        ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+    OIDS=FALSE,
+    autovacuum_enabled=true
+);
+
+ALTER TABLE public.messages
+    OWNER TO silizium;
